7057b406367775086296335513ce6a9a6a72eaf3
Jed Eland <jedeland@live.co.uk>
2022-02-16 23:09:08 +0100

    TLDR: switch cases are annoying in go ...

--- ourventure-backend/RestApi/OurVentureClass/Relation.go
+++ ourventure-backend/RestApi/OurVentureClass/Relation.go
@@ -1,5 +1,20 @@
 package OurVentureClass
 
+
 type Relation struct {
-	RelationsList [] string
+	RelationsList []string
+}
+
+type RelationType struct {
+	RelationTypeName  string
+	RelationTypeValue float64
+}
+
+func (rel *RelationType) CalculateRelationship() {
+	possibleTypes := []string{"Atrocious", "Terrible", "Bad", "Worsening",
+		"Neutral", "Positive", "Good",
+		"Favorable", "Exceptional"}
+	if rel.RelationTypeValue >= -4.5 && rel.RelationTypeValue <= -6.0{
+		rel.RelationTypeName = possibleTypes[0]
+	}
 }
