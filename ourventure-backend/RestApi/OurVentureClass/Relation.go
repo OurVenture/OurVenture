150fa79fea61e2645cec296c4a8f63bdad1239d6
Jed Eland <jedeland@live.co.uk>
2022-02-18 22:44:08 +0100

    Trying out if else, will try case now

--- ourventure-backend/RestApi/OurVentureClass/Relation.go
+++ ourventure-backend/RestApi/OurVentureClass/Relation.go
@@ -1,5 +1,8 @@
 package OurVentureClass
 
+import (
+	"math"
+)
 
 type Relation struct {
 	RelationsList []string
@@ -14,7 +17,16 @@ func (rel *RelationType) CalculateRelationship() {
 	possibleTypes := []string{"Atrocious", "Terrible", "Bad", "Worsening",
 		"Neutral", "Positive", "Good",
 		"Favorable", "Exceptional"}
-	if rel.RelationTypeValue >= -4.5 && rel.RelationTypeValue <= -6.0{
+	relNum := math.Round(rel.RelationTypeValue*10) / 10
+	if relNum >= -4.5 && relNum <= -6.0{
 		rel.RelationTypeName = possibleTypes[0]
+	} else if relNum >= -3.5 && relNum <= -4.4 {
+		rel.RelationTypeName = possibleTypes[1]
+	} else if relNum >= -2.0 && relNum <= -3.4 {
+		rel.RelationTypeName = possibleTypes[2]
+	} else if relNum >= -0.5 && relNum <= -1.9 {
+		rel.RelationTypeName = possibleTypes[3]
+	} else if relNum >= -0.4 && relNum <= 0.4 {
+		rel.RelationTypeName = possibleTypes[4]
 	}
-}
+ }
